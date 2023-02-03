7c4e92bf7f4813e27f47046e1f705be5a5827018
Matti Luukkainen <mluukkai@iki.fi>
2023-02-03 16:34:06 +0200

    fix

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -12,7 +12,9 @@ const PersonForm = ({ setError }) => {
   const [ createPerson ] = useMutation(CREATE_PERSON, {
     refetchQueries: [ { query: ALL_PERSONS } ],
     onError: (error) => {
-      setError(error.graphQLErrors[0].message)
+      const errors = error.graphQLErrors[0].extensions.error.errors
+      const messages = Object.values(errors).map(e => e.message).join('\n')
+      setError(messages)
     }
   })
 
