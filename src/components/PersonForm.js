db707c00e784adc4c72335b9f4001dea691032a4
xzhc <1838998336@qq.com>
2023-08-26 17:41:51 +0800

    User login && Adding a token to a header

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -21,7 +21,10 @@ const PersonForm = ({ setError }) => {
   const submit = async (event) => {
     event.preventDefault()
 
-    createPerson({  variables: { name, phone, street, city } })
+    createPerson({  variables: { name, street, city,
+        phone: phone.length > 0 ? phone : undefined 
+      } 
+  })
 
     setName('')
     setPhone('')
